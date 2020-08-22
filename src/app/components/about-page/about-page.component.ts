27ca314a3dddf6568214300b75e784504c44618b
Gavin de Bruyn <gavin@mitnet.co.za>
2020-08-22 09:32:13 +0200

    Improved styling on the About page. Created ChangeLog model. About page uses an array of ChangeLogs to dynamically generate the change log section on the about page.
    Signed-off-by: Gavin de Bruyn <gavindb@runninghill.co.za>

--- src/app/components/about-page/about-page.component.ts
+++ src/app/components/about-page/about-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ChangeLog} from '../../models/ChangeLog';
 
 @Component({
   selector: 'app-about-page',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about-page.component.scss']
 })
 export class AboutPageComponent implements OnInit {
+  changeLogs: ChangeLog[] = [
+    new ChangeLog('1.0.0', '27 Apr 2020', ['Upload your miner\'s config file.', 'Enable/Disable algorithms for each GPU'], []),
+    new ChangeLog('1.1.0', '22 August 2020', ['Created About page', 'Enabling/Disabling an algorithm for one GPU enables/disables the algorithm for the rest.'], [])
+  ];
 
   constructor() { }
 
