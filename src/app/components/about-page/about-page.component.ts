bf978fc83bb7ae314ac42a2f7a1acb20ab4aa53f
Gavin de Bruyn <gavin@mitnet.co.za>
2020-12-06 12:26:48 +0200

    Updated change log and fixed styling on the about page.
    Signed-off-by: Gavin de Bruyn <gavindb@runninghill.co.za>

--- src/app/components/about-page/about-page.component.ts
+++ src/app/components/about-page/about-page.component.ts
@@ -8,8 +8,9 @@ import {ChangeLog} from '../../models/ChangeLog';
 })
 export class AboutPageComponent implements OnInit {
   changeLogs: ChangeLog[] = [
-    new ChangeLog('1.0.0', '27 Apr 2020', ['Upload your miner\'s config file.', 'Enable/Disable algorithms for each GPU'], []),
-    new ChangeLog('1.1.0', '22 August 2020', ['Created About page', 'Enabling/Disabling an algorithm for one GPU enables/disables the algorithm for the rest.'], [])
+    new ChangeLog('1.2.0', '6 December 2020', ['Added a setting so that the user can decide if they want to perform actions for all GPUs or not.', 'Added support for overclocking.', 'Algorithm names are now displayed instead of IDs for a better user experience.'], ['Fixed device_settings.json not parsing correctly for certain cards.']),
+    new ChangeLog('1.1.0', '22 August 2020', ['Created About page.', 'Enabling/Disabling an algorithm for one GPU enables/disables the algorithm for the rest.'], []),
+    new ChangeLog('1.0.0', '27 Apr 2020', ['Upload your miner\'s config file.', 'Enable/Disable algorithms for each GPU.'], []),
   ];
 
   constructor() { }
